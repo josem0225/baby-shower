b2686f87e5d095d144641a45dbdae210dd0d6c0e
Jose Miguel Rozo Baez <josem407r@gmail.com>
2026-08-26 22:10:13 -0500

    Update location to Torres de Hayuelos

--- components/EventDetails.tsx
+++ components/EventDetails.tsx
@@ -44,10 +44,10 @@ export function EventDetails() {
               <MapPin size={24} strokeWidth={1.5} />
             </div>
             <h3 className="font-serif text-xl text-cloud-800 mb-2">Lugar</h3>
-            <p className="font-sans text-silver-500">Conjunto Residencial La Isla</p>
-            <p className="font-sans text-silver-500 text-sm mt-1 mb-4">Salón Social</p>
+            <p className="font-sans text-silver-500">Conjunto Residencial Torres de Hayuelos</p>
+            <p className="font-sans text-silver-500 text-sm mt-1 mb-4">Cra. 92 #17B-35, Bogotá</p>
             <a 
-              href="https://www.google.com/maps?sca_esv=630f4fbcc449dd86&rlz=1C5OZZY_enCO1130CO1130&output=search&q=conjunto+residencial+la+isla&source=lnms&fbs=ABfTbFVX0tIkpKPae7RXR9SKovMxO22odM34wfevGG3a5XcKdfI5Ec0jWtT3iZ6F0cnWKaSNWkyKuP0iY1b3g98bGX6ApHULZbpGPMUSZ3t6Pp1zypeiDCHXyazuT6snICQ17wFo2BrrT9THpaQgGAvoHo7paKQ5YqmlFslsoibvdSVptZ6xRcnxeu8KYYBH_rYt8Ao6jrGZtkS9236dJTROej8pJjs5zQ&entry=mc&ved=1t:200715&ictx=111" 
+              href="https://www.google.com/maps/search/?api=1&query=Conjunto+Residencial+Torres+de+Hayuelos+Bogota" 
               target="_blank" 
               rel="noopener noreferrer"
               className="w-full"
